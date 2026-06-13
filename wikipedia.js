8569b4c9b9bb934077b99145b18d2b0bb930dcd9
vrivoire <rivoire.vincent@gmail.com>
2026-06-13 09:50:25 -0400

    New files

---
+++ wikipedia.js
@@ -0,0 +1,44 @@
+chrome.runtime.onMessage.addListener( data => {
+  if ( data.type === 'notification' ) {
+          chrome.notifications.create(
+              '',
+              {
+                  type: 'basic',
+                  title: 'Notify!',
+                  message: data.message || 'Notify!',
+                  iconUrl: './assets/icons/128.png',
+              }
+          );
+  }
+});
+
+chrome.runtime.onMessage.addListener( data => {
+  if ( data.type === 'notification' ) {
+    notify( data.message );
+  }
+});
+
+chrome.runtime.onInstalled.addListener( () => {
+  chrome.contextMenus.create({
+    id: 'notify',
+	title: "Recherchez Rotten Tomatoes pour '%s'",
+	contexts: [ "selection" ]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener( ( info, tab ) => {
+  if ( 'notify' === info.menuItemId ) {
+    doSearch( info.selectionText, tab );
+  }
+} );
+
+function doSearch (search_target,tab)
+{
+	chrome.tabs.create( {
+		url : "https://www.rottentomatoes.com/search?search="+encodeURIComponent(search_target),
+		selected : true,
+		index : tab.index + 1
+	} );
+};
+
+
